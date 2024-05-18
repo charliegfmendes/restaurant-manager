f16fb19ba66aaacf72b9b0eebf6fdd6da6e2d4c9
Gabriel <gabrielrfmendes@gmail.com>
2024-05-18 10:40:14 -0300

    chore: configuração do prettier e eslint

--- App.tsx
+++ App.tsx
@@ -2,19 +2,42 @@ import * as React from 'react';
 import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+function HomeScreen({
+  navigation,
+}: StackNavigationProp<RootStackParamList, 'Home'>) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <Text>Home Screen</Text>
-      <Button title="Navigate to details" onPress={() => navigation.navigate('Details')} />
+      <Button
+        title="Navigate to details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
 
 function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <Text>Details Screen</Text>
     </View>
   );
